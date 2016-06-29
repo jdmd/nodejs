53f29d30999aaf696bef6593c0f6177445132707
Andrei Osipau <andrei.ossipoff@gmail.com>
2016-06-29 22:06:49 +0300

    ignore node_modules

--- js-exp/ex1/ex1.js
+++ js-exp/ex1/ex1.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var opn = require('opn');
 var port = process.env.PORT || 3000;
 
-http.createServer(function(req,res) {
+server = http.createServer(function(req,res) {
 	res.setHeader('Content-Type', 'text/html');
 	res.writeHeader(200);
 	res.write('Hello from jdmd!');
