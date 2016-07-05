928b79517aed6bdcc79dbc6f43d2ec9e1b9c9a64
Andrei Osipau <andrei.ossipoff@gmail.com>
2016-07-05 14:16:14 +0300

    ex6: chat is edited

--- js-exp/ex6/chat/_index.js
+++ js-exp/ex6/chat/_index.js
@@ -7,6 +7,15 @@ var io = socket.listen(app.listen(port, function() {
     console.log('App listen port:', port);
 }));
 
+var users = {};
+function getUsers(obj) {
+	var tmp = [];
+	for (var i in obj) {
+		tmp.push(obj[i]);
+	}
+	return tmp.join(', ');
+}
+
 app.set('views', __dirname + '/tpl');
 app.set('view engine', 'pug');
 app.engine('pug', require('pug').__express);
@@ -19,12 +28,30 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', function(client) {
     console.log('Connected');
-    client.emit('message', { message: "Добро пожаловать в чат!" });
+    //client.emit('message', { message: "Добро пожаловать в чат!" });
     client.on('send', function(data) {
         if (data.message) {
-            io.sockets.emit('message', { message: data.message });
+            io.sockets.emit('message', { message: data.message, name: data.name });
         } else {
             io.sockets.emit('message', { message: 'Ошибка отправления' });
         }
+    });
+    client.on('hello', function(data) {
+        client.nickname = data.name;
+        client.emit('message', { message: '--- Добро пожаловать в чат, ' + data.name + '! ---' });
+        client.broadcast.emit('message', { message: '--- ' + data.name + ' присоединился к чату ---' });
+        if (Object.keys(users).length > 0) {
+        	var userList = getUsers(users);
+        	client.emit('message', {message: '--- Уже в чате: '+userList+' ---'});
+        } else {
+        	client.emit('message', {message: '--- Кроме вас в чате никого нет :( ---'});
+        }
+        users[client.id] = data.name;
+    });
+    client.on('disconnect', function(data) {
+    	if (Object.keys(users).length > 1) {
+    		client.broadcast.emit('message', {message: '--- '+client.nickname+' покинул чат ---'});
+    	}
+    	delete users[client.id];
     });
 });
--- js-exp/ex6/chat/public/chat.js
+++ js-exp/ex6/chat/public/chat.js
@@ -1,26 +1,42 @@
+var socket;
+
 window.onload = function() {
-	var socket = io.connect('http://localhost:8080');
-	var messages = [];
-	var field = document.getElementById('field');
-	var form = document.getElementById('form');
-	var content = document.getElementById('content');
+    socket = io.connect('http://localhost:8080');
+    var messages = [];
+    var field = document.getElementById('field');
+    var form = document.getElementById('form');
+    var content = document.getElementById('content');
 
-	socket.on('message', function(data) {
-		if(data.message){
-			messages.push(data.message);
-			var html = '';
-			for(var i=0; i<messages.length; i++) {
-				html += messages[i] + '<br/>';
-			}
-			content.innerHTML = html;
-		} else {
-			console.log('Something wrong.');
-		}
-	});
+    var name = prompt('Как вас зовут?', 'Гость');
 
-	form.onsubmit = function() {
-		var text = field.value;
-		socket.emit('send', {message: text});
-		return false;
-	};
+    if (name) {
+        socket.emit('hello', { name: name });
+    }
+
+    socket.on('message', function(data) {
+        if (data.message) {
+            if (data.name) {
+                data.message = data.name + ': ' + data.message;
+            }
+            messages.push(data.message);
+            var html = '';
+            for (var i = 0; i < messages.length; i++) {
+                html += messages[i] + '<br/>';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log('Something wrong.');
+        }
+    });
+
+    form.onsubmit = function() {
+        var text = field.value;
+        socket.emit('send', { message: text, name: name });
+        form.reset();
+        return false;
+    };
 };
+
+window.onunload = function() {
+    socket.disconnect();
+}
